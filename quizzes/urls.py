bc6efbfde2453e4666adfebc84fe473e725a183e
root <root@Robin.(none)>
2014-01-28 20:02:38 -0500

    fixed urls, views and templates, stuck on fixing quiz title template/view

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import patterns, url
 from quizzes import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<Quiz_Title>.+)/$', views.quiz, name='quiz'),
+
 )
 
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from quizzes.models import Quiz
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
-# Create your views here.
+    quiz_list = Quiz.objects.all()
+    template = loader.get_template('quizzes/index.html')
+    context = {'quiz_list': quiz_list}
+    return render(request, 'quizzes/index.html', context)
+def quiz(request,Quiz_Title):
+    #quiz_request = Quiz.objects.get(Quiz_Title=quiz)
+    template = loader.get_template('quizzes/quiz.html')
+    context = {'Quiz_Title': Quiz_Title}
+    return render(request, 'quizzes/quiz.html', context)  
+   # Create your views here.
