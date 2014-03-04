68f7445a6527eb0d4088ef5be5c8fe1e08f36cf0
root <root@Robin.(none)>
2014-03-04 00:28:30 -0500

    modified templates and views to let results of quiz be computed

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^create/created', views.created, name = 'created'),
     url(r'^create/$', views.quiz_create, name='quiz_create'),
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<Quiz_Name>.+)/$', views.quiz, name='quiz'),
+	url(r'^(?P<Quiz_Name>.+)/result', views.result, name='result'),
+	url(r'^(?P<Quiz_Name>.+)/$', views.quiz, name='quiz'),
 )
 
--- quizzes/views.py
+++ quizzes/views.py
@@ -14,7 +14,7 @@ def index(request):
     template = loader.get_template('quizzes/index.html')
     context = {'quiz_list': quiz_list}
     return render(request, 'quizzes/index.html', context)
-
+@csrf_protect
 def quiz(request,Quiz_Name):
     quiz_request = Quiz.objects.get(Quiz_Title=Quiz_Name)
     quiz_questions = []
@@ -27,71 +27,85 @@ def quiz(request,Quiz_Name):
 
 @csrf_protect
 def quiz_create(request):
-    c = {}
-    c.update(csrf(request))
-    template = loader.get_template('quizzes/quiz_create.html')
+	c = {'request' : request}
+	c.update(csrf(request))
+	template = loader.get_template('quizzes/quiz_create.html')
     #return render(request,'quizzes/quiz_create.html')
-    return render_to_response("quizzes/quiz_create.html", c)
+	return render_to_response("quizzes/quiz_create.html", c)
 
 @csrf_protect
+def result(request,Quiz_Name):
+	quiz = Quiz.objects.get(Quiz_Title=Quiz_Name)
+	results = []
+	for r in quiz.results.all():
+		if (r != ','):
+			results.append(r)
+	c = {'request' : request, 'results' : results}
+	c.update(csrf(request))
+	template = loader.get_template('quizzes/result.html')
+	print results
+	return render_to_response('quizzes/result.html',c)
+@csrf_protect
 def created(request):
-    #process_Quiz(request)
-    data = request.POST.dict()
-
-    #print results
-    template = loader.get_template('quizzes/quiz_created.html')
+    
+	data = request.POST.dict()
+	template = loader.get_template('quizzes/quiz_created.html')
     #here we do some stuff to add the quiz, questions and results to our DB
-    quiz = Quiz()
-    quiz.Quiz_Title = request.POST['Quiz_Name']
-    quiz.Quiz_Description = request.POST['Quiz_Description']
-	#quiz.save()
-    quiz.save()
-	
-    #here's the tricky part, we need to add the questions and results
-    #first we count the number of questions
-	#this can be used to loop through all the questions then save them
-	#question_1 = results.get('question_1',None)
-    total_questions = 0
-    total_results = 0
-    for key in data:
+	quiz = Quiz()
+
+	quiz.Quiz_Title = request.POST['Quiz_Name']
+	quiz.Quiz_Description = request.POST['Quiz_Description']
+
+	quiz.save()
+
+	Results_List = data['All_Results']
+	Results_Explanation_List = data['All_Results_Explanation']
+	total_questions = 0
+	total_results = 0
+	for key in data:
 		#this deals with the questions
-        if (re.match(r'question\d',key)):
-            total_questions = total_questions+1
-            question = Question()
-            question.question_text = data.__getitem__(key)
-            question.answer1 = data['answer1_q' + str(total_questions)]
-            question.answer2 = data['answer2_q' + str(total_questions)]
-            question.answer3 = data['answer3_q' + str(total_questions)]
-            question.answer4 = data['answer4_q' + str(total_questions)]
-            question.save()
-            quiz.questions.add(question)
-            quiz.save()
+		if (re.match(r'question\d',key)):
+			total_questions = total_questions+1
+			question = Question()
+			question.question_text = data[key]
+			question.answer1 = data['answer1_q' + str(total_questions)]
+			question.answer2 = data['answer2_q' + str(total_questions)]
+			question.answer3 = data['answer3_q' + str(total_questions)]
+			question.answer4 = data['answer4_q' + str(total_questions)]
+			question.save()
+			quiz.questions.add(question)
+			quiz.save()
 	for key in data:
 		#this deals with results
-		if (re.match(r'result\d*$',key)):
-			print key
-			total_results = total_results +1
+		if (re.match(r'r1.*',key)):
+			#result scoring is of the form rx_y_z where x is the answer
+			#y is the result and z is the question
 			result = Result()
-			result.save()
-			result.Quiz_Result = data[key]
-			result_num = key[6:]
-			result_number = "result_explanation" + result_num
-			result_number = string.strip(result_number,"'")
-			result_number = string.strip(result_number,'"')
-			result.Quiz_Result_Explanation = data[result_number]
-			#this is the hard part, getting the scoring for each result
-			#each question has four numbers, we use the appropriate index to get them
-			scoring_list = [None] * (4 * (total_questions + 1))
-			for x in range(1, 4):
-				scoring_list[0] = data['r1_' + result_num]
-				scoring_list[1] = data['r2_' + result_num]
-				scoring_list[2] = data['r3_' + result_num]
-				scoring_list[3] = data['r4_' + result_num]
+			#we need to find the location of result because it could have more than one digit
+			left_index = string.find(key,'_')
+			right_index = string.rfind(key,'_')
+			result_number = key[left_index+1:right_index]
+			question_number = key[right_index+1:]
+			r_list = Results_List.split(",")
+			r_e_list = Results_Explanation_List.split(",")
+			result.Quiz_Result = r_list[int(result_number)-1]
+			result.Quiz_Result_Explanation = r_e_list[int(result_number)-1]
+			scoring_list = []
+			for match_key in data:
+				if (re.match(r'r1_'+result_number+'.*',match_key)):
+						#this takes us through all the scores for a particular results
+						result_scoring_list = []
+						result_scoring_list.append(data[match_key])
+						#we take the end of match_key but replace the begining with r2
+						result_scoring_list.append(data['r2'+match_key[2:]])
+						result_scoring_list.append(data['r3'+match_key[2:]])
+						result_scoring_list.append(data['r4'+match_key[2:]])
+						scoring_list.append(result_scoring_list)
 			result.Quiz_Scoring = scoring_list
 			result.save()
+			#right now we're adding this twice
 			quiz.results.add(result)
 			quiz.save()
-	
 	quiz_questions = []
 	quiz_results = []
 	for q in quiz.questions.all():
@@ -101,4 +115,4 @@ def created(request):
 		if (r != ','):
 			quiz_results.append(r)
 	context = {"results": data, "quiz_questions": quiz_questions, "quiz_results": quiz_results}
-    return render(request,'quizzes/quiz_created.html',context) 
+	return render(request,'quizzes/quiz_created.html',context) 
