374c9d1e4729ba624ff4fe664c41fe0ff8f59c28
root <root@Robin.(none)>
2014-03-18 13:07:03 -0400

    revamped models.py

--- quizmaker/settings.py
+++ quizmaker/settings.py
@@ -80,5 +80,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+MEDIA_ROOT = '/images/'
+MEDIA_URL = '/'
 STATIC_URL = '/static/'
--- quizzes/models.py
+++ quizzes/models.py
@@ -6,19 +6,24 @@ class Quiz(models.Model):
 	questions = models.ManyToManyField('Question', null=True, blank=True)
 	#Quiz_Questions = models.CommaSeparatedIntegerField(max_length=100)
 	results = models.ManyToManyField('Result', null=True, blank=True)
+	image = models.ImageField(Quiz_Title,100,100)
+	date = models.DateTimeField(auto_now=True)
+	timesTaken = models.IntegerField()
 	def __unicode__(self):
 		        return unicode(self.questions)
+	
 class Question(models.Model):
 	question_text = models.CharField(max_length=500)
-	answer1 = models.CharField(max_length=50)
-	answer2 = models.CharField(max_length=50)
-	answer3 = models.CharField(max_length=50)
-	answer4 = models.CharField(max_length=50)
-	#result = models.ManyToManyField('Result')
-	##perhaps I'm getting ahead of myself here
-	def get_answers(self):
-		return ( (self.answer1, '1'),(self.answer2, '2'),(self.answer3,'3'),(self.answer4,'4'))
-	#selectedAnswer = models.CharField(max_length=50,choices=Answers)
+	num_answers = models.IntegerField()
+	questionNumber = models.IntegerField()
+	#this should place the image in quiz/question_pk
+	image = models.ImageField(Quiz.objects.get(questions=self) + '/' + self.pk)
+
+class Answer(models.Model):
+	question = models.ForeignKey('Question',null=True,blank=True)
+	answertext = models.CharField(max_length=100)
+	#this should place the image in quiz/question_pk/answer_pk
+	image = models.ImageField(Quiz.objects.get(questions=question) + '/' + question.pk + '/' + Answer.pk)
 	def __unicode__(self):
 		        return unicode(self.question_text)
 	#Quiz_Part = models.ForeignKey(Quiz,default=1)
@@ -26,6 +31,8 @@ class Result(models.Model):
 	#Quiz_Results = models.ForeignKey(Quiz,default=1)
 	Quiz_Scoring = models.CommaSeparatedIntegerField(max_length=100)
 	Quiz_Result = models.CharField(max_length=100)
+	resultNumber = models.IntegerField()
 	def __unicode__(self):
 		        return unicode(self.Quiz_Result)
 	Quiz_Result_Explanation = models.CharField(max_length=1000)
+	image = models.ImageField(Quiz.objects.get(results=self) + '/' + Quiz_Result)
