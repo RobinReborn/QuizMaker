241eda55f7cd08a28acacdf8129ed027ddb3c21a
root <root@Robin.(none)>
2014-03-24 12:54:22 -0400

    fixed models, other minor changes

--- quizzes/migrations/0001_initial.py
+++ quizzes/migrations/0001_initial.py
@@ -13,6 +13,10 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('Quiz_Title', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('Quiz_Description', self.gf('django.db.models.fields.CharField')(max_length=1000)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('timesTaken', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('Quiz_Type', self.gf('django.db.models.fields.CharField')(max_length=1000)),
         ))
         db.send_create_signal(u'quizzes', ['Quiz'])
 
@@ -38,28 +42,29 @@ class Migration(SchemaMigration):
         db.create_table(u'quizzes_question', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('question_text', self.gf('django.db.models.fields.CharField')(max_length=500)),
-            ('answer1', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('answer2', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('answer3', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('answer4', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('num_answers', self.gf('django.db.models.fields.IntegerField')()),
+            ('questionNumber', self.gf('django.db.models.fields.IntegerField')()),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
         db.send_create_signal(u'quizzes', ['Question'])
 
-        # Adding M2M table for field result on 'Question'
-        m2m_table_name = db.shorten_name(u'quizzes_question_result')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('question', models.ForeignKey(orm[u'quizzes.question'], null=False)),
-            ('result', models.ForeignKey(orm[u'quizzes.result'], null=False))
+        # Adding model 'Answer'
+        db.create_table(u'quizzes_answer', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['quizzes.Question'], null=True, blank=True)),
+            ('answertext', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
-        db.create_unique(m2m_table_name, ['question_id', 'result_id'])
+        db.send_create_signal(u'quizzes', ['Answer'])
 
         # Adding model 'Result'
         db.create_table(u'quizzes_result', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('Quiz_Scoring', self.gf('django.db.models.fields.CommaSeparatedIntegerField')(max_length=100)),
             ('Quiz_Result', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('resultNumber', self.gf('django.db.models.fields.IntegerField')()),
             ('Quiz_Result_Explanation', self.gf('django.db.models.fields.CharField')(max_length=1000)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
         db.send_create_signal(u'quizzes', ['Result'])
 
@@ -77,38 +82,49 @@ class Migration(SchemaMigration):
         # Deleting model 'Question'
         db.delete_table(u'quizzes_question')
 
-        # Removing M2M table for field result on 'Question'
-        db.delete_table(db.shorten_name(u'quizzes_question_result'))
+        # Deleting model 'Answer'
+        db.delete_table(u'quizzes_answer')
 
         # Deleting model 'Result'
         db.delete_table(u'quizzes_result')
 
 
     models = {
+        u'quizzes.answer': {
+            'Meta': {'object_name': 'Answer'},
+            'answertext': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['quizzes.Question']", 'null': 'True', 'blank': 'True'})
+        },
         u'quizzes.question': {
             'Meta': {'object_name': 'Question'},
-            'answer1': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'answer2': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'answer3': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'answer4': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'question_text': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
-            'result': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['quizzes.Result']", 'symmetrical': 'False'})
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'num_answers': ('django.db.models.fields.IntegerField', [], {}),
+            'questionNumber': ('django.db.models.fields.IntegerField', [], {}),
+            'question_text': ('django.db.models.fields.CharField', [], {'max_length': '500'})
         },
         u'quizzes.quiz': {
             'Meta': {'object_name': 'Quiz'},
             'Quiz_Description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
             'Quiz_Title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'Quiz_Type': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'questions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['quizzes.Question']", 'null': 'True', 'blank': 'True'}),
-            'results': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['quizzes.Result']", 'null': 'True', 'blank': 'True'})
+            'results': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['quizzes.Result']", 'null': 'True', 'blank': 'True'}),
+            'timesTaken': ('django.db.models.fields.IntegerField', [], {'default': '0'})
         },
         u'quizzes.result': {
             'Meta': {'object_name': 'Result'},
             'Quiz_Result': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'Quiz_Result_Explanation': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
             'Quiz_Scoring': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'resultNumber': ('django.db.models.fields.IntegerField', [], {})
         }
     }
 
--- quizzes/models.py
+++ quizzes/models.py
@@ -2,28 +2,30 @@ from django.db import models
 class Quiz(models.Model):
 	Quiz_Title = models.CharField(max_length=100)
 	Quiz_Description = models.CharField(max_length=1000)
-	#each of these is paired to the id of a question
+	#each of these is paired to a question
 	questions = models.ManyToManyField('Question', null=True, blank=True)
-	#Quiz_Questions = models.CommaSeparatedIntegerField(max_length=100)
 	results = models.ManyToManyField('Result', null=True, blank=True)
-	image = models.ImageField(Quiz_Title,100,100)
+	image = models.ImageField(upload_to='/images/')
 	date = models.DateTimeField(auto_now=True)
-	timesTaken = models.IntegerField()
+	timesTaken = models.IntegerField(default=0)
+	Quiz_Type = models.CharField(max_length=1000)
 	def __unicode__(self):
-		        return unicode(self.questions)
+		return unicode(self.questions)
 	
 class Question(models.Model):
 	question_text = models.CharField(max_length=500)
-	num_answers = models.IntegerField()
-	questionNumber = models.IntegerField()
+	num_answers = models.IntegerField(default=0)
+	questionNumber = models.IntegerField(default=0)
 	#this should place the image in quiz/question_pk
-	image = models.ImageField(Quiz.objects.get(questions=self) + '/' + self.pk)
+	#def imageLocation(self):
+	#	return Quiz.objects.get(questions=self) + '/' + self.pk
+	image = models.ImageField(upload_to='/images/')
 
 class Answer(models.Model):
 	question = models.ForeignKey('Question',null=True,blank=True)
 	answertext = models.CharField(max_length=100)
 	#this should place the image in quiz/question_pk/answer_pk
-	image = models.ImageField(Quiz.objects.get(questions=question) + '/' + question.pk + '/' + Answer.pk)
+	image = models.ImageField(upload_to='/images/')
 	def __unicode__(self):
 		        return unicode(self.question_text)
 	#Quiz_Part = models.ForeignKey(Quiz,default=1)
@@ -35,4 +37,4 @@ class Result(models.Model):
 	def __unicode__(self):
 		        return unicode(self.Quiz_Result)
 	Quiz_Result_Explanation = models.CharField(max_length=1000)
-	image = models.ImageField(Quiz.objects.get(results=self) + '/' + Quiz_Result)
+	image = models.ImageField(upload_to='/images/')
