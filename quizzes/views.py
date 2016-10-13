1775eac2be62360428a6a8ccaefa215701e350f1
robin <robinganemccalla@gmail.com>
2016-10-13 16:23:08 -0400

    modified results of taking a quiz, now user sees all the answers with the correct one highlighted in green and if they were incorrect they see their incorrect answer highlited in red

--- quizzes/views.py
+++ quizzes/views.py
@@ -8,34 +8,56 @@ from django.views.decorators import csrf
 from django.shortcuts import render_to_response
 import re
 import string
-#from helper_functions import process_Quiz
 from django.http import QueryDict
 from django.template.loader import get_template
 
 
 def index(request):
-	print "in index"
 	quiz_list = Quiz.objects.all()
 	context = {'quiz_list': quiz_list}
 	return render(request, 'quizzes/index.html', context)
-#@csrf_protect
 def quiz(request,Quiz_Name):
 	quiz_request = Quiz.objects.get(Quiz_Title=Quiz_Name)
 	quiz_questions = []
 	quiz_answers = []
-	quiz_results = []
 	for q in Question.objects.filter(quiz=quiz_request).order_by('questionNumber'):
 		if (q != ','):
 			quiz_questions.append(q)
-			#right now we're going through all the answers
-			#we could change the model to have answers belong to questions
-			#rather than the other way around
 			for a in Answer.objects.filter(question=q).order_by('answerNumber'):
 				quiz_answers.append(a)
 	template = loader.get_template('quizzes/quiz.html')
 	context = {'quiz_request': quiz_request, 'quiz_questions': quiz_questions, 'quiz_answers': quiz_answers}
 	return render(request, 'quizzes/quiz.html', context)  
-#@csrf_protect
+def result(request,Quiz_Name):
+	data = request.POST.dict()
+	correctAnswers = 0
+	quiz_questions = []
+	quiz_answers = []
+	correctAnswerArray = []
+	compareAnswerArray = []
+	userAnswerArray = []
+	quiz = Quiz.objects.get(Quiz_Title=Quiz_Name)
+	for q in Question.objects.filter(quiz=quiz).order_by('questionNumber'):
+		if (q != ','):
+			quiz_questions.append(q)
+			for a in Answer.objects.filter(question=q).order_by('answerNumber'):
+				quiz_answers.append(a)
+	#here we go through all the questions, find the correct answer and see if that matches the user input
+	for z in range(1,quiz.question_set.count()+1):
+		q = quiz.question_set.get(questionNumber=z)
+		correctAnswer = q.answer_set.get(correctAnswer="True").answerNumber
+		correctAnswerArray.append(int(correctAnswer))
+		userAnswerArray.append(int(data["question-"+str(z)]))
+		#if (int(data["question-"+str(z)]) == correctAnswer):
+		#	correctAnswers = correctAnswers+1
+		#	correctAnswerArray.append("1")
+		#else:
+		#	correctAnswerArray.append("0")
+	compareAnswerArray = zip(correctAnswerArray,userAnswerArray)
+	print compareAnswerArray	
+	c = {'correctAnswers' : correctAnswers,'quiz_questions': quiz_questions, 'quiz_answers': quiz_answers, 'compareAnswerArray': compareAnswerArray}
+	template = loader.get_template('quizzes/result.html')
+	return render(request,'quizzes/result.html',c)
 def quiz_create(request):
 	c = {'request' : request}
 	template = loader.get_template('quizzes/quiz_create.html')
@@ -43,11 +65,12 @@ def quiz_create(request):
 def add_answers(request):	
 	template = loader.get_template('quizzes/quiz_create.html')
 	data = request.POST.dict()
-	QuestionArray = data["questionArray"].split(",")
 	quiz = Quiz()
 	quiz.Quiz_Title = request.POST['Quiz_Name']
 	quiz.Quiz_Description = request.POST['Quiz_Description']
 	quiz.save()
+	QuestionArray = data["questionArray"].split(",")
+	#we create a question for each question in teh array
 	for x in range(0, len(QuestionArray)):
 		question = Question()
 		question.question_text = QuestionArray[x]
@@ -59,22 +82,24 @@ def add_answers(request):
 	return render(request,"quizzes/add_answers.html", context)
 def add_results(request):
 	data= request.POST.dict()
-	#dataList = []
 	questionArray = data['Questions']
+	#first we split up each set of answers
 	answerArray = data['AnswersArray'].split(",/,")
+	#then we split up the individual answers
 	for x in range(0,len(answerArray)):
 		answerArray[x] = answerArray[x].split(",")
+	#we need to get rid of the last element in the array because of the way things are split
 	del answerArray[-1][-1]
 	quiz = Quiz.objects.get(Quiz_Title=data['Quiz_Name'])
 	quizType = data['quizType']	
 	for x in range(0,len(answerArray)):
-		#print answerArray[x] + '\n'
-		#print answerArray[x][0]
 		correctAnswer = int(answerArray[x][0])
+		#we delete the index of the correct answer as we do not create an object for it
 		del answerArray[x][0]
 		questionNumber = x+1
 		question_add = quiz.question_set.get(questionNumber=questionNumber)
 		for a in range(0,len(answerArray[x])):
+			#for each answer we add information, then add it to the appropriate question
 			answer = Answer()
 			if (a == correctAnswer):
 				answer.correctAnswer = "True"
@@ -85,44 +110,9 @@ def add_results(request):
 			answer.save()
 			question_add.answer_set.add(answer)
 		question_add.save()
-	#for key in data:
-	#	if (re.match(r'q\d+a\d+',key)):
-	#		answerIndex = re.findall(r'\d+',key)
-	#		answer = Answer()
-	#		question_number = int(answerIndex[0])
-	#		answer.answerNumber = answerIndex[1]
-	#		answer.answertext = data[key]
-	#		correctAnswer = data["q" + answerIndex[0] + "a"]
-	#		if (answerIndex[1]== correctAnswer):
-	#			answer.correctAnswer = "True"
-	#		else:
-	#			answer.correctAnswer = "False"
-	#		answer.save()
-	#		question_add = quiz.question_set.get(questionNumber=question_number)
-	#		question_add.answer_set.add(answer)
-	#		question_add.save()
-			#question_add.answers.order_by(answerNumber)
-	#	dataList.append({data[key],key})
 	quiz.save()
 	context = {'quizType': quizType, 'quiz': quiz}
 	return render(request,'quizzes/add_results.html',context)
-#@csrf_protect
-def result(request,Quiz_Name):
-	data = request.POST.dict()
-	correctAnswers = 0
-	quiz = Quiz.objects.get(Quiz_Title=Quiz_Name)
-	for z in range(1,quiz.question_set.count()+1):
-		q = quiz.question_set.get(questionNumber=z)
-		correctAnswer = q.answer_set.get(correctAnswer="True").answerNumber
-		print str(correctAnswer) + " " + data["question-"+str(z)] + "\n"
-		if (int(data["question-"+str(z)]) == correctAnswer):
-			print "correct answer!\n"
-			correctAnswers = correctAnswers+1	
-	c = {'request' : request, 'correctAnswers' : correctAnswers}
-	template = loader.get_template('quizzes/result.html')
-	#print results
-	return render(request,'quizzes/result.html',c)
-#@csrf_protect
 def created(request):
 	print "in created"
 	data = request.POST.dict()
@@ -132,111 +122,8 @@ def created(request):
 	quiz.Quiz_Title = request.POST['Quiz_Name']
 	quiz.Quiz_Description = request.POST['Quiz_Description']
 	quiz.save()
-	#Results_List = data['All_Results']
-	#Results_Explanation_List = data['All_Results_Explanation']
-	#questionAnswersArray = data['questionAnswersArray']
-	#q_a_a_list = questionAnswersArray.split(",")
-	#r_list = Results_List.split(",")
-	#r_e_list = Results_Explanation_List.split(",")
 	total_questions = int(data['numQuestions'])
-	#total_results = int(data['numResults'])
-	#questions =1
-	#for key in data:
-		#this deals with the questions
-		
-	#	if (re.match(r'question\d',key)):
-	#		question = Question()
-	#		question.question_text = data[key]
-	#		question.questionNumber = questions
-	#		question.num_Answers = q_a_a_list[questions-1]
-	#		question.save()
-	#		quiz.questions.add(question)
-	#		questions = questions+1
 	quiz.save()
-	#for key in data:
-		#this deals with answers
-	#	if (re.match(r'answer.*',key)):
-	#		answer = Answer()
-	#		answer.answertext = data[key]
-			#print data[key]
-			#we parse the key to find the question and answer number
-	#		answerNumber = key[6:string.find(key,'_')]
-	#		questionNumber = key[string.find(key,'_') + 2:]
-	#		answer.answerNumber = answerNumber
-	#		#maybe not the most efficient way of doing this
-	#		question_add = quiz.questions.get(questionNumber=questionNumber)
-	#		answer.save()
-	#		question_add.answers.add(answer)
-	#		question_add.save()
-			#question_add.answers.order_by(answerNumber)
-	#we need to order the answers
-	#result_number = 0
-	#for x in range(1,total_results+1):
-	#	quiz_scoring = []
-	#	result = Result()
-	#	result.Quiz_Result = r_list[x-1]
-	#	print r_list[x-1]
-	#	result.Quiz_Result_Explanation = r_e_list[x-1]
-	#	for y in range(1,total_questions+1):
-	#		score_list = []
-	#		for z in range(0,(int(q_a_a_list[y]) )):
-	#			score_list.append(data['r'+str(y)+'_'+str(x)+'_'+str(z+1)])
-	#		quiz_scoring.append(score_list)
-	#	result.Quiz_Scoring = quiz_scoring
-	#	result.resultNumber = x
-		#result.Quiz_Result = 
-	#	result.save()
-	#	print result.Quiz_Result
-	#	quiz.results.add(result)
-	#	quiz.save()
-	#for key in data:
-	#	result_scoring_list=[]
-	#	total_scoring_list=[]
-	#	result = Result()
-		#we can rely in having a score value for r1_y_z, then look through y and z
-	#	if (re.match(r'r1.*',key)):
-			#result scoring is of the form rx_y_z where x is the answer
-			#y is the result and z is the question
-	#		result = Result()
-			#we need to find the location of result because it could have more than one digit
-	#		left_index = string.find(key,'_')
-	#		right_index = string.rfind(key,'_')
-	#		result_number = key[left_index+1:right_index]
-	#		question_number = key[right_index+1:]
-	#		result.Quiz_Result = r_list[int(result_number)-1]
-	#		result.Quiz_Result_Explanation = r_e_list[int(result_number)-1]
-			#total_scoring_list = []
-			#this deals with a specific answer in the results
-			#right now the scoring list doesn't contain all the information it needs to
-			#for match_key in data:
-			#if (re.match(r'r1_'+result_number+'.*',match_key)):
-			#this takes us through all the scores for a particular results
-	#		result_scoring_list = []
-			#result_scoring_list.append(data[match_key])
-			#for each answer in the question
-	#		for x in range(0,(int(q_a_a_list[int(question_number)]) )):
-				#print x
-	#			print "appending to" + question_number
-	#			print 'r'+str(x+1)+key[left_index:]
-	#			print data['r'+str(x+1)+'_'+result_number+'_'+question_number]
-	#			result_scoring_list.append(data['r'+str(x+1)+'_'+result_number+'_'+question_number])
-	#		total_scoring_list.append(result_scoring_list)
-	#		result.Quiz_Scoring = total_scoring_list
-		#print result_number
-		#print total_scoring_list
-	#		result_number = int(result_number)
-	#		result.resultNumber = result_number
-	#		result.save()
-	#		result_number = result_number+1
-		#this needs to be modified
-		#we only add a result for the first question
-		#if (re.match(r'r1_'+result_number+'_1',key)):
-		#right now we're adding this for each question
-	#		quiz.results.add(result)
-	#		print "saving result"
-	#		print result.resultNumber
-	#		print result.Quiz_Scoring
-	#		quiz.save()
 	quiz_questions = []
 	quiz_results = []
 	for q in quiz.questions.all():
